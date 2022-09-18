f13b31829dfddd4e1d81680e8d472083872cd756
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-09-18 17:25:11 -0300

    Add comments to scriptjs

--- script.js
+++ script.js
@@ -3,25 +3,33 @@ const ctx = canvas.getContext("2d"); // context
 // console.log(ctx);
 const CANVAS_WIDTH = (canvas.width = 600); // same as in style.css
 const CANVAS_HEIGHT = (canvas.height = 600); // same as in style.css
-
+// Load frames from sprite sheet
 const playerImage = new Image(); // Same as img tag on html
 playerImage.src = "shadow_dog.png";
 
+// Set frames sizes
 const spriteWidth = 575; // width of image file divided by number of columns (frames)
 const spriteHeight = 523; // height of image file divided by number of rows (frames)
 let playerState = "idle";
 
+// Set frame speed
 let gameFrame = 0;
 const staggerFrames = 5;
+
+// Store positions on sprite sheet for each frame
 const spriteAnimations = [];
 
+// Get number of frames for each state
 const getAnimationStates = await fetch("animation-states.json");
 const animationStates = await getAnimationStates.json();
+
+// State dropdown
 const dropdown = document.getElementById("animations");
 dropdown.addEventListener("change", (event) => {
   playerState = event.target.value;
 });
 
+// Create positions for each frame
 animationStates.forEach((state, index) => {
   let frames = {
     location: [],
@@ -42,6 +50,7 @@ animationStates.forEach((state, index) => {
 });
 // console.log(spriteAnimations);
 
+// Animation loop
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   // ctx.fillRect(50, 50, 100, 100);
@@ -64,6 +73,7 @@ function animate() {
     spriteHeight // destination height
   );
 
+  // Advance a game frame
   gameFrame++;
   requestAnimationFrame(animate); // Animation loop
 }
